6bb4b9f3f73e167fb3c637d52f23382ded38f555
Michael Frantz <mjfrantz26@gmail.com>
2020-05-30 13:11:40 -0500

    Fixed icons in portfolio and added links

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Banner from './component/Banner/Banner';
 import About from './component/About';
 import mikeData from './component/mikeData';
+import Portfolio from './component/Portfolio/Portfolio';
 
 export const Home = () => (
   <div className="body_wrapper">
     <Banner mikeData={mikeData} bClass="banner_shap" imag="mike.jpeg" textLeft="text-left" />
     <About aClass="about_area" mikeData={mikeData} />
+    <Portfolio />
   </div>
 )
