0f68230239943fe5d4da54685ade45382fcf9d38
Michael Frantz <mjfrantz26@gmail.com>
2020-06-08 23:22:39 -0500

    Updated resume

--- src/component/About.js
+++ src/component/About.js
@@ -9,7 +9,7 @@ class About extends Component {
     let mikeData = this.props.mikeData;
 
     const handlePDFOpen = () => {
-      window.open('./resume.pdf', '_blank', 'fullscreen=yes')
+      window.open('./resume1.pdf', '_blank', 'fullscreen=yes')
     }
 
     return (
