1451a66985f59bc20a5abb40d1fe4ebaed68102f
Michael Frantz <mjfrantz26@gmail.com>
2020-06-09 16:52:27 -0500

    fixed banner

--- src/component/Banner/Banner.js
+++ src/component/Banner/Banner.js
@@ -13,10 +13,10 @@ class Banner extends Component {
             <div className="container">
               <div className={`banner_content ${textLeft}`}>
                 <Reveal effect="fadeInUp">
-                  <h5>Hello</h5>
+                  <h5>Hello,</h5>
                 </Reveal>
                 <Reveal effect="fadeInUp" duration={1000}><h2 className="wow fadeInLeft animated">I am {mikeData.name}</h2></Reveal>
-                <Reveal effect="fadeInUp" duration={1500}><h4 className="wow fadeInUp animated">Fullstack Developer</h4></Reveal>
+                <Reveal effect="fadeInUp" duration={1500}><h4 className="wow fadeInUp animated">Fullstack Web Developer</h4></Reveal>
                 <ul className="list_style social_icon">
                   {
                     mikeData.socialLinks && mikeData.socialLinks.map(item => {
